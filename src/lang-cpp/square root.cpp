74ef7e1d30f896fc4d5a26ce0acb2ff79b6e60cb
Aya Abdel Slam <yoyolovelygirl93@yahoo.com>
2014-07-19 21:44:46 +0200

    square root
    program to calculate a square root of any number.

---
+++ src/lang-cpp/square root.cpp
@@ -0,0 +1,11 @@
+/* square root example */
+
+include /* printf */
+include /* sqrt */
+int main () { 
+double param, result;
+ param = 1024.0;
+ result = sqrt (param);
+ printf ("sqrt(%f) = %f\n", param, result ); 
+return 0;
+ }
