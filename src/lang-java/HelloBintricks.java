694246ba44bc8c5d6249e460e4177e5241ef35f0
Omar Gawish <omer.gawish@live.com>
2014-07-23 05:38:13 +0200

    add new class to lang-java

---
+++ src/lang-java/HelloBintricks.java
@@ -0,0 +1,16 @@
+
+package hellobintricks;
+
+/**
+ *
+ * @author omer
+ */
+public class HelloBintricks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello ,Bintricks :D");
+    }
+}
